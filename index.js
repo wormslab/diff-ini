54cb8b8fb64beb51805298a8aff677e8fb65eddd
wormslab <wormslab@gmail.com>
2015-11-20 06:02:58 +0900

    improve: user can input options

--- index.js
+++ index.js
@@ -3,11 +3,30 @@
 
   let fs = require('fs')
     , reader = require('./lib/iniReader')
-    , differ = require('./lib/diffini');
+    , differ = require('./lib/diffini')
+    , defaultOpt = require('./lib/defaultOptions');
 
-  module.exports = function(ls, rs, options) {
-    let ld = reader(ls);
-    let rd = reader(rs);
+  module.exports = function(ls, rs, inputOptions) {
+    let defaultOptions = {
+      caseInsensitive: true,
+      section: {
+        allowDuplicate: false
+      },
+      key: {
+        allowDuplicate: false,
+        replcaeDuplicate: false
+      },
+      value: {
+        hasDelimiter: true,
+        needToSort: true,
+        delimiter: ','
+      }
+    }
+
+    let options = defaultOpt(defaultOptions, inputOptions);
+    console.log(options);
+    let ld = reader(ls, options);
+    let rd = reader(rs, options);
     return differ(ld, rd);
   }
 })();
--- test.js
+++ test.js
@@ -7,11 +7,28 @@
 
   let ls = fs.readFileSync('./sample.ini');
   let rs = fs.readFileSync('./sample-diff.ini');
+  let options = {
+    caseInsensitive: true,
+    section: {
+      allowDuplicate: false
+    },
+    key: {
+      allowDuplicate: false,
+      replaceDuplicate: false
+    },
+    value: {
+      hasDelimiter: true,
+      needToSort: true,
+      delimiter: ','
+    }
+  }
+  console.log('USED OPTIONS------------------------------');
+  console.log(options);
   console.log('LEFT SIDE --------------------------------');
   console.log(ls.toString());
   console.log('RIGHT SIDE--------------------------------');
   console.log(rs.toString());
   console.log('RESULT --------------------------------');
-  let ret = index(ls.toString(), rs.toString());
+  let ret = index(ls.toString(), rs.toString(), options);
   console.log(util.inspect(ret, { depth: null }));
 })();
